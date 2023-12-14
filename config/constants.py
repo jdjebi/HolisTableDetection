63c032772866088ac7a5856ac5c3dc915b24d20c
jdjebi <jeanmarcdjebi@gmail.com>
2023-12-14 19:04:55 +0100

    Ajout de nouvelles constantes

--- config/constants.py
+++ config/constants.py
@@ -2,6 +2,17 @@ from pathlib import Path
 
 ENCODING = "utf-8-sig"
 
+# Sous-dossier d'un dossier de sortie
+CSV_DIR = "csv"
+__SCAN_TABLES_PAGE_IMAGES_DIR = "scan_table_page_images"
+__IMAGE_TABLE_DETECTED_DIR = "image_table_detected"
+
+PDF_RAW_TEXT_CSV_FILE = "pdf_raw_text.csv"
+__SCAN_TABLES_PAGE_IMAGES_CSV = "scan_table_page_images.csv"
+__SCAN_TABLES_IMAGES_CSV = "scan_table_images.csv"
+__DATASET_CSV = "table_dataset.csv"
+# ---
+
 DATASET_DIR = Path("datasets") / "csv"
 DATASET_SAVE_PATH = (DATASET_DIR / "pdf_raw_text.csv").absolute().resolve()
 IMAGE_TABLE_DETECTED_DIR = Path("datasets/image_table_detected").absolute().resolve()
@@ -14,8 +25,8 @@ SCAN_TABLES_IMAGES_CSV = Path("datasets/csv/scan_table_images.csv").absolute().r
 OUTPUT_CSV = Path("datasets/csv/scan_table_page_images.csv").absolute().resolve()
 DATASET_CSV = Path("datasets/csv/table_dataset.csv").absolute().resolve()
 
-INFERENCE_SCRIPT = Path("table-transformer/src/inference.py").absolute().resolve()
-MODEL_PATH = Path("table-transformer/model/pubtables1m_detection_detr_r18.pth").absolute().resolve()
-DETECTION_CONFIG_PATH = Path("table-transformer/src/detection_config.json").absolute().resolve()
+INFERENCE_SCRIPT = Path("table_transformer/src/inference.py").absolute().resolve()
+MODEL_PATH = Path("table_transformer/model/pubtables1m_detection_detr_r18.pth").absolute().resolve()
+DETECTION_CONFIG_PATH = Path("table_transformer/src/detection_config.json").absolute().resolve()
 
 TESSERACT_EXECUTABLE = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
