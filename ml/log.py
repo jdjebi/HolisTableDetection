21186bf55da16130ab3dfc795ba01426fb28fdc9
jdjebi <jeanmarcdjebi@gmail.com>
2023-12-19 02:54:20 +0100

    Modification du nombre features à afficher

--- ml/log.py
+++ ml/log.py
@@ -4,6 +4,6 @@ import numpy as np
 def show_coef(coef, features_names):
     coef_abs = abs(coef)
     features_index_desc = np.argsort(coef_abs)[::-1]
-    lr_best_index = features_index_desc[:10]
+    lr_best_index = features_index_desc[:5]
     for index in lr_best_index:
         print(f' feature : {index} :', {features_names[index]})
